0e9b072dcbf9746efb348bf4c3b42d834e7b0499
salinaaaaaa <joehoeller@gmail.com>
2022-09-06 17:09:14 -0500

    Delete s3.py

--- s3.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def init_aws_creds(run_id):
-    os.environ["AWS_ACCESS_KEY_ID"] = "AKIA2XJ25AX67PK5VAO6"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
-    
-    
-
